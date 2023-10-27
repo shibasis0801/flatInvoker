d06e99634c8c5572373bd1d519e787b15a304dc9
shibasis <13601918+shibasis0801@users.noreply.github.com>
2023-10-28 00:06:34 +0530

    remove useless folders

--- cmake-build-debug/_deps/flatbuffers-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ovd/CLionProjects/bifrost/cmake-build-debug/_deps/flatbuffers-src
-# Build directory: /Users/ovd/CLionProjects/bifrost/cmake-build-debug/_deps/flatbuffers-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[flattests]=] "/Users/ovd/CLionProjects/bifrost/cmake-build-debug/_deps/flatbuffers-build/flattests")
-set_tests_properties([=[flattests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/ovd/CLionProjects/bifrost/cmake-build-debug/_deps/flatbuffers-src/CMakeLists.txt;680;add_test;/Users/ovd/CLionProjects/bifrost/cmake-build-debug/_deps/flatbuffers-src/CMakeLists.txt;0;")
