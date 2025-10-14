2929dfcb63e89168aedf6311a5bef84c5d2bbe4f
Shibasis <shibasis0801@gmail.com>
2025-10-15 00:50:18 +0530

    progress, need to finish the rpc mechanisms

--- reaktor-flexbuffer/cpp/common/Visitor.cpp
+++ reaktor-flexbuffer/cpp/common/Visitor.cpp
@@ -1,13 +1,14 @@
 #include <common/CppBase.h>
 
 
+class Pet;
 class Cat;
 class Dog;
 
 class PetVisitor {
 public:
-    virtual void visit(Cat* c) = 0;
-    virtual void visit(Dog* d) = 0;
+    virtual void visit(Cat* c, Pet* p) = 0;
+    virtual void visit(Dog* d, Pet* p) = 0;
 };
 
 
@@ -15,13 +16,20 @@ public:
 class Pet {
 public:
     virtual ~Pet() {}
-    virtual void accept(PetVisitor& visitor) = 0;
+    virtual void accept(PetVisitor& visitor, Pet* p = nullptr) = 0;
 
     Pet(string_view color): color_(color) {}
     const string& color() const { return color_; }
+    void add_child(Pet *baby) {
+        children.push_back(baby);
+    }
+    const vector<Pet*>& get_children() const {
+        return children;
+    }
 
 private:
     const string color_;
+    vector<Pet*> children;
 };
 
 
@@ -29,8 +37,8 @@ class Cat: public Pet {
 public:
     Cat(string_view color): Pet(color) {}
 
-    void accept(PetVisitor &visitor) override {
-        visitor.visit(this);
+    void accept(PetVisitor &visitor, Pet* p = nullptr) override {
+        visitor.visit(this, p);
     }
 
 };
@@ -39,8 +47,8 @@ class Dog: public Pet {
 public:
     Dog(string_view color): Pet(color) {}
 
-    void accept(PetVisitor &visitor) override {
-        visitor.visit(this);
+    void accept(PetVisitor &visitor, Pet *p = nullptr) override {
+        visitor.visit(this, p);
     }
 };
 
@@ -48,30 +56,312 @@ public:
 #include <iostream>
 class FeedingVisitor: public PetVisitor {
 public:
-    void visit(Cat *c) override {
+    void visit(Cat *c, Pet *p = nullptr) override {
         std::cout << "Feeding cat: " << c->color() << std::endl;
     }
 
-    void visit(Dog *d) override {
+    void visit(Dog *d, Pet *p = nullptr) override {
         std::cout << "Feeding dog: " << d->color() << std::endl;
     }
 };
 
 class PlayingVisitor: public PetVisitor {
 public:
-    void visit(Cat *c) override {
+    void visit(Cat *c, Pet *p = nullptr) override {
         std::cout << "Playing cat: " << c->color() << std::endl;
     }
 
-    void visit(Dog *d) override {
+    void visit(Dog *d, Pet *p = nullptr) override {
         std::cout << "Playing dog: " << d->color() << std::endl;
     }
 };
 
-void tester() {
+class BabyCreationVisitor: public PetVisitor {
+public:
+    void visit(Cat *c, Pet *p) override {
+        assert(dynamic_cast<Cat*>(p));
+        c->add_child(p);
+    }
+
+    void visit(Dog *d, Pet *p) override {
+        assert(dynamic_cast<Dog*>(p));
+        d->add_child(p);
+    }
+};
+
+class CensusVisitor: public PetVisitor {
+public:
+    void visit(Cat *c, Pet *p = nullptr) override {
+        std::cout << c-> color();
+        for (auto *baby: c->get_children()) {
+            baby->accept(*this);
+        }
+    }
+
+    void visit(Dog *d, Pet *p = nullptr) override {
+        std::cout << d-> color();
+        for (auto *baby: d->get_children()) {
+            baby->accept(*this);
+        }
+    }
+};
+
+void PetTester() {
     auto pet = unique_ptr<Pet>(new Cat("orange"));
     FeedingVisitor fv;
 
     pet->accept(fv);
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Visitor;
+
+class Geometry {
+public:
+    virtual ~Geometry() {}
+    virtual void accept(Visitor &v) = 0;
+    enum type_tag { POINT = 100, CIRCLE, LINE, INTERSECTION };
+    virtual type_tag tag() const = 0;
+
+    static Geometry* make_geometry(Geometry::type_tag &tag);
+
+};
+
+class Point: public Geometry {
+public:
+    Point() = default;
+    Point(double x, double y): x(x), y(y) {}
+
+    void accept(Visitor &v) override;
+
+    type_tag tag() const override { return POINT; };
+
+private:
+    double x, y;
+};
+
+class Circle: public Geometry {
+public:
+    Circle() = default;
+    Circle(Point centre, double radius): centre(centre), radius(radius) {}
+
+    void accept(Visitor &v) override;
+    type_tag tag() const override { return CIRCLE; };
+private:
+    Point centre;
+    double radius;
+};
+
+class Line: public Geometry {
+public:
+    Line() = default;
+    Line(Point start, Point end): start(start), end(end) {}
+
+    void accept(Visitor &v) override;
+    type_tag tag() const override { return LINE; };
+private:
+    Point start;
+    Point end;
+};
+
+class Intersection: public Geometry {
+public:
+    Intersection() = default;
+    Intersection(Geometry *g1, Geometry *g2): g1(g1), g2(g2) {}
+
+    type_tag tag() const override {
+        return INTERSECTION;
+    }
+    void accept(Visitor &v) override;
+
+private:
+    std::unique_ptr<Geometry> g1;
+    std::unique_ptr<Geometry> g2;
+};
+
+class Visitor {
+public:
+    virtual void visit(double& d) = 0;
+    virtual void visit(Point& p) = 0;
+    virtual void visit(Circle& c) = 0;
+    virtual void visit(Line& l) = 0;
+    virtual void visit(Geometry::type_tag& tag) = 0;
+
+    static Geometry* make_geometry(Geometry::type_tag tag) {
+        switch (tag) {
+            case Geometry::POINT: return new Point();
+            case Geometry::LINE: return new Line();
+            case Geometry::CIRCLE: return new Circle();
+            case Geometry::INTERSECTION: return new Intersection();
+        }
+    }
+};
+
+
+void Point::accept(Visitor &v) {
+    v.visit(x);
+    v.visit(y);
+}
+
+void Circle::accept(Visitor &v) {
+    v.visit(centre);
+    v.visit(radius);
+}
+
+void Line::accept(Visitor &v) {
+    v.visit(start);
+    v.visit(end);
+}
+
+void Intersection::accept(Visitor &v) {
+    Geometry::type_tag tag;
+
+    if (g1) {
+        tag = g1->tag();
+        v.visit(tag);
+        g1->accept(v);
+    }
+
+    if (g2) {
+        tag = g2->tag();
+        v.visit(tag);
+        g2->accept(v);
+    }
+}
+
+class StringSerializerVisitor: public Visitor {
+public:
+    void visit(double& d) override {
+        S << d << " ";
+    }
+
+    void visit(Point& p) override {
+        p.accept(*this);
+    }
+
+    void visit(Circle& c) override {
+        c.accept(*this);
+    }
+
+    void visit(Line& l) override {
+        l.accept(*this);
+    }
+
+    void visit(Geometry::type_tag &tag) override {
+        S << size_t(tag) << " ";
+    }
+
+    string str() {
+        return S.str();
+    }
+
+private:
+    std::stringstream S;
+};
+
+
+class StringDeserializationVisitor: public Visitor {
+public:
+    StringDeserializationVisitor(string s) {
+        S.str(s);
+    }
+
+    void visit(double& d) override {
+        S >> d;
+    }
+
+    void visit(Point& p) override {
+        p.accept(*this);
+    }
+
+    void visit(Circle& c) override {
+        c.accept(*this);
+    }
+
+    void visit(Line& l) override {
+        l.accept(*this);
+    }
+
+    void visit(Geometry::type_tag &tag) override {
+        size_t value;
+        S >> value;
+        tag = Geometry::type_tag(value);
+    }
+
+    string str() {
+        return S.str();
+    }
+
+private:
+    std::stringstream S;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- reaktor-navigation/src/commonMain/kotlin/dev/shibasis/reaktor/navigation/Graph.kt
+++ reaktor-navigation/src/commonMain/kotlin/dev/shibasis/reaktor/navigation/Graph.kt
@@ -5,18 +5,24 @@ import dev.shibasis.reaktor.core.framework.Dispatch
 import dev.shibasis.reaktor.core.framework.Feature
 import dev.shibasis.reaktor.io.network.RoutePattern
 import dev.shibasis.reaktor.navigation.koin.Koin
+import kotlinx.atomicfu.AtomicBoolean
 import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.drop
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import org.koin.core.parameter.ParametersDefinition
 import org.koin.core.qualifier.Qualifier
 import org.koin.core.qualifier.named
@@ -29,9 +35,7 @@ import kotlin.js.JsExport
 import kotlin.reflect.KClass
 import kotlin.uuid.Uuid
 
-
 // todo add https://github.com/turansky/seskar for kotlin/js sugar
-
 /*
 
 A reactive graph library for application nodes inspired from Uber/RIBs and Actor frameworks like Akka/Erlang.
@@ -64,14 +68,19 @@ ApiNode
 This needs to be concurrent to make use of all cores properly.
 */
 
-
 // Your close() must be idempotent and can be called multiple times.
 interface Capability: AutoCloseable {
-
 }
 
 inline operator fun<reified T: Capability> T.invoke(fn: T.() -> Unit) = fn(this)
 inline operator fun<reified T: Capability> T.invoke() = this
+
+
+interface Unique {
+    val id: Uuid
+}
+
+class UniqueImpl(override val id: Uuid = Uuid.random()): Unique
 
 @JsExport
 sealed class Lifecycle {
@@ -119,10 +128,9 @@ class LifecycleCapabilityImpl: LifecycleCapability {
 object ReaktorScope {
     val Graph = named("Reaktor.Graph")
     val GraphNode = named("Reaktor.GraphNode")
-    val ApiNode = named("Reaktor.ApiNode")
+    val LogicNode = named("Reaktor.LogicNode")
     val RouteNode = named("Reaktor.RouteNode")
     val ViewNode = named("Reaktor.ViewNode")
-    val InteractorNode = named("Reaktor.InteractorNode")
 }
 
 
@@ -148,14 +156,15 @@ class DependencyCapabilityImpl(
     }
 
     init {
-        parentScope?.linkTo()
         parentScope?.let { koinScope.linkTo(it) }
+        // todo no thread safety
         Feature.Koin.load(module)
     }
 
     override fun close() {
         Feature.Koin.unload(module)
         parentScope?.let { koinScope.unlink(it) }
+        koinScope.close()
     }
 }
 
@@ -175,7 +184,7 @@ interface ConcurrencyCapability: Capability {
 @JsExport
 class ConcurrencyCapabilityImpl(
     context: CoroutineContext? = null,
-    dispatcher: CoroutineDispatcher = Dispatchers.Default
+    dispatcher: CoroutineDispatcher
 ): ConcurrencyCapability {
 
     val supervisorJob = SupervisorJob()
@@ -187,14 +196,10 @@ class ConcurrencyCapabilityImpl(
     )
 
     override fun close() {
-        supervisorJob.cancel(CancellationException("Reaktor:AutoCloseable"))
+        coroutineScope.cancel(CancellationException("Reaktor:AutoCloseable"))
     }
 }
 
-
-interface Unique {
-    val id: Uuid
-}
 
 @JsExport
 interface SealedRegistryCapability<SealedType: Unique> : MutableMap<Uuid, SealedType>, Capability {
@@ -250,19 +255,21 @@ class SealedRegistryCapabilityImpl<SealedType: Unique>(
 @JsExport
 open class Graph(
     parentGraph: Graph? = null,
+    dispatcher: CoroutineDispatcher,
     override val id: Uuid = Uuid.random(),
     val dependency: ScopedDependency
 ):
     Unique,
     LifecycleCapability by LifecycleCapabilityImpl(),
     DependencyCapability by DependencyCapabilityImpl(id.toString(), ReaktorScope.Graph, parentGraph?.koinScope, dependency),
-    ConcurrencyCapability by ConcurrencyCapabilityImpl(parentGraph?.coroutineScope?.coroutineContext),
+    ConcurrencyCapability by ConcurrencyCapabilityImpl(parentGraph?.coroutineScope?.coroutineContext, dispatcher),
     SealedRegistryCapability<Node> by SealedRegistryCapabilityImpl()
 {
     override fun close() {
         this<LifecycleCapability> { close() }
         this<DependencyCapability> { close() }
         this<ConcurrencyCapability> { close() }
+        this<SealedRegistryCapability<Node>> { close() }
     }
 }
 
@@ -270,26 +277,32 @@ open class Graph(
 sealed class Node(
     val parent: Graph,
     koinQualifier: Qualifier,
+    dispatcher: CoroutineDispatcher = Dispatchers.Default,
     val dependency: ScopedDependency = {},
     override val id: Uuid = Uuid.random(),
 ):
     Unique,
     LifecycleCapability by LifecycleCapabilityImpl(),
     DependencyCapability by DependencyCapabilityImpl(id.toString(), koinQualifier, parent.koinScope, dependency),
-    ConcurrencyCapability by ConcurrencyCapabilityImpl(parent.coroutineScope.coroutineContext),
-    SealedRegistryCapability<DirectedEdge> by SealedRegistryCapabilityImpl()
+    ConcurrencyCapability by ConcurrencyCapabilityImpl(parent.coroutineScope.coroutineContext, dispatcher),
+    SealedRegistryCapability<DirectedEdge<*, *>> by SealedRegistryCapabilityImpl()
 {
     override fun close() {
         this<LifecycleCapability> { close() }
         this<DependencyCapability> { close() }
         this<ConcurrencyCapability> { close() }
+        this<SealedRegistryCapability<DirectedEdge<*, *>>> { close() }
+    }
+
+    infix fun<Request, Response> addEdge(other: Node) {
+
     }
 }
 
 
 /*
 A GraphNode is responsible to expose functionality from inside of a Graph to external users through edge factories.
- */
+*/
 @JsExport
 open class GraphNode(
     val graph: Graph,
@@ -299,9 +312,9 @@ open class GraphNode(
 }
 
 @JsExport
-abstract class ApiNode(
+abstract class LogicNode(
     graph: Graph
-): Node(graph, ReaktorScope.ApiNode) {
+): Node(graph, ReaktorScope.LogicNode) {
 
 }
 
@@ -310,21 +323,18 @@ abstract class RouteNode(
     val pattern: RoutePattern,
     graph: Graph
 ): Node(graph, ReaktorScope.RouteNode) {
-    fun getViewNode(): ViewNode<*, *>? {
-        return edges
-            .firstOrNull { it.end is ViewNode<*, *> }
-            ?.let { it.end as ViewNode<*, *> }
-    }
+//    fun getViewNode(): ViewNode<*, *>? {
+//        return getAllOfType<ViewNode<*, *>>()?.firstOrNull()
+//    }
 }
 
 @JsExport
-abstract class ViewNode<Input, Signal>(
+abstract class ViewNode<State>(
     // mandated initial input for previews.
-    previewInput: Input
-): LifecycleCapability by LifecycleCapabilityImpl() {
-
-    val state = MutableStateFlow(previewInput)
-    val signal = MutableSharedFlow<Signal>()
+    previewState: State,
+    graph: Graph
+): Node(graph, ReaktorScope.ViewNode) {
+    val state = MutableStateFlow(previewState)
 
     var isPreview = true
         private set
@@ -336,9 +346,128 @@ abstract class ViewNode<Input, Signal>(
         }
     }
 
-
-    // You call your composable / component through this.
+    // Call your composable / component through this.
     open fun render() {}
+}
+
+/**
+ * Using gRPC semantics, https://grpc.io/docs/what-is-grpc/core-concepts/#service-definition
+ * gRPC lets you define four kinds of service method:
+ *
+ * Unary RPCs where the client sends a single request to the server and gets a single response back, just like a normal function call.
+ *
+ * rpc SayHello(HelloRequest) returns (HelloResponse);
+ * Server streaming RPCs where the client sends a request to the server and gets a stream to read a sequence of messages back. The client reads from the returned stream until there are no more messages. gRPC guarantees message ordering within an individual RPC call.
+ *
+ * rpc LotsOfReplies(HelloRequest) returns (stream HelloResponse);
+ * Client streaming RPCs where the client writes a sequence of messages and sends them to the server, again using a provided stream. Once the client has finished writing the messages, it waits for the server to read them and return its response. Again gRPC guarantees message ordering within an individual RPC call.
+ *
+ * rpc LotsOfGreetings(stream HelloRequest) returns (HelloResponse);
+ * Bidirectional streaming RPCs where both sides send a sequence of messages using a read-write stream. The two streams operate independently, so clients and servers can read and write in whatever order they like: for example, the server could wait to receive all the client messages before writing its responses, or it could alternately read a message then write a message, or some other combination of reads and writes. The order of messages in each stream is preserved.
+ *
+ * rpc BidiHello(stream HelloRequest) returns (stream HelloResponse);
+ *
+ */
+sealed class Command<Request, Response> {
+    data class UnarySync<Request, Response>(
+        val request: Request,
+        val response: Response
+    ): Command<Request, Response>()
+
+    data class Unary<Request, Response>(
+        val request: Request,
+        val response: CompletableDeferred<Response>
+    ): Command<Request, Response>()
+
+    data class ResponseStream<Request, Response>(
+        val request: Request,
+        val response: Channel<Response>
+    ): Command<Request, Response>()
+
+    data class RequestStream<Request, Response>(
+        val request: Channel<Request>,
+        val response: CompletableDeferred<Response>
+    ): Command<Request, Response>()
+
+    data class BidirectionalStream<Request, Response>(
+        val request: Channel<Request>,
+        val response: Channel<Response>
+    ): Command<Request, Response>()
+}
+
+sealed class DirectedEdge<Request, Response>(
+    val start: Node,
+    val end: Node,
+    val outgoing: MutableSharedFlow<Command<Request, Response>> = MutableSharedFlow(),
+): Unique by UniqueImpl() {
+
+    suspend fun unary(request: Request): Response {
+        val response = CompletableDeferred<Response>()
+        val command = Command.Unary(request, response)
+        outgoing.emit(command)
+        return response.await()
+    }
+
+    suspend fun responseStream(request: Request): Channel<Response> {
+        val response = Channel<Response>()
+        val command = Command.ResponseStream(request, response)
+        outgoing.emit(command)
+        return response
+    }
+
+    suspend fun requestStream(request: Channel<Request>): Response {
+        val response = CompletableDeferred<Response>()
+        val command = Command.RequestStream(request, response)
+        outgoing.emit(command)
+        return response.await()
+    }
+
+    suspend fun responseStream(request: Channel<Request>): Channel<Response> {
+        val response = Channel<Response>()
+        val command = Command.BidirectionalStream(request, response)
+        outgoing.emit(command)
+        return response
+    }
+}
+
+class UnaryDirectedEdge<Request, Response>(
+    start: Node, end: Node
+): DirectedEdge<Request, Response>(start, end)
+
+class DuplexDirectedEdge<
+    ForwardRequest, ForwardResponse,
+    ReverseRequest, ReverseResponse>(
+        start: Node,
+        end: Node,
+        val incoming: MutableSharedFlow<Command<ReverseRequest, ReverseResponse>> = MutableSharedFlow()
+): DirectedEdge<ForwardRequest, ForwardResponse>(start, end) {
+    suspend fun unaryReverse(request: ReverseRequest): ReverseResponse {
+        val response = CompletableDeferred<ReverseResponse>()
+        val command = Command.Unary(request, response)
+        incoming.emit(command)
+        return response.await()
+    }
+
+    suspend fun responseStreamReverse(request: ReverseRequest): Channel<ReverseResponse> {
+        val response = Channel<ReverseResponse>()
+        val command = Command.ResponseStream(request, response)
+        incoming.emit(command)
+        return response
+    }
+
+    suspend fun requestStreamReverse(request: Channel<ReverseRequest>): ReverseResponse {
+        val response = CompletableDeferred<ReverseResponse>()
+        val command = Command.RequestStream(request, response)
+        incoming.emit(command)
+        return response.await()
+    }
+
+    suspend fun responseStreamReverse(request: Channel<ReverseRequest>): Channel<ReverseResponse> {
+        val response = Channel<ReverseResponse>()
+        val command = Command.BidirectionalStream(request, response)
+        incoming.emit(command)
+        return response
+    }
 }
 
 /*
@@ -350,32 +479,48 @@ We will use Visitor pattern to traverse the graph as needed.
 3.
  */
 open class Visitor {
-    open fun visit(node: Node) {}
-}
+    open fun visit(graph: Graph) {}
 
-sealed class DirectedEdge(
-    open val start: Node,
-    open val end: Node,
-    override val id: Uuid = end.id
-): Unique {
-    class ConnectionEdge(
-        override val start: Node,
-        override val end: Node
-    ): DirectedEdge(start, end)
+    open fun visit(graphNode: GraphNode) {}
 
-    class ChannelEdge<Message>(
-        override val start: Node,
-        override val end: Node,
-        val channel: Channel<Message> = Channel()
-    ): DirectedEdge(start, end)
+    open fun visit(logicNode: LogicNode) {}
+    open fun visit(routeNode: RouteNode) {}
+    open fun visit(viewNode: ViewNode<*>) {}
 
-    class FlowEdge<Message>(
-        override val start: Node,
-        override val end: Node,
-        val flow: Flow<Message>
-    ): DirectedEdge(start, end)
+    open fun visit(edge: DirectedEdge<*, *>) {}
 }
 
 
+sealed interface HomeScreenCommand {
+    data object ApiRequest: HomeScreenCommand
+}
 
+class HomeScreenNode(
+    graph: Graph,
+    val edge: UnaryDirectedEdge<HomeScreenCommand, HomeInteractorResponse>
+): ViewNode<Unit>(Unit, graph) {
 
+    fun t() {
+        coroutineScope.launch {
+            val x = edge.unary(HomeScreenCommand.ApiRequest)
+            val c = Channel<HomeScreenCommand>()
+            val y = edge.requestStream(c)
+
+        }
+    }
+}
+
+sealed interface HomeInteractorResponse {
+    data object ApiResponse: HomeInteractorResponse
+}
+
+class HomeInteractorNode(graph: Graph): LogicNode(graph) {
+
+}
+
+fun t(graph: Graph) {
+    val screen = HomeScreenNode(graph)
+    val interactor = HomeInteractorNode(graph)
+
+    val edge = UnaryDirectedEdge<HomeScreenCommand, HomeInteractorResponse>(screen, interactor)
+}
